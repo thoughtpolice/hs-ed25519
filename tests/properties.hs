d1d7899aabff1038cbab3a41bee47777e52f171d
Austin Seipp <aseipp@pobox.com>
2015-09-28 01:28:54 -0500

    Deprecate sign'/verify' for d{sign,verify}
    I think these names are just a little bit better, long term. Easier to search
    for, too.
    
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- tests/properties.hs
+++ tests/properties.hs
@@ -38,24 +38,24 @@ roundtrip xs
 
 roundtrip' :: ByteString -> Property
 roundtrip' xs
-  = keypairProp $ \(pk,sk) -> verify' pk xs (sign' sk xs)
+  = keypairProp $ \(pk,sk) -> dverify pk xs (dsign sk xs)
 
 -- Generally the signature format is '<signature><original message>'
 -- and <signature> is of a fixed length (crypto_sign_BYTES), which in
--- ed25519's case is 64. sign' drops the message appended at the end,
--- so we just make sure we have constant length signatures.
+-- ed25519's case is 64. @'dsign'@ drops the message appended at the
+-- end, so we just make sure we have constant length signatures.
 signLength :: (ByteString,ByteString) -> Property
 signLength (xs,xs2)
   = keypairProp $ \(_,sk) ->
-      let s1 = unSignature $ sign' sk xs
-          s2 = unSignature $ sign' sk xs2
+      let s1 = unSignature $ dsign sk xs
+          s2 = unSignature $ dsign sk xs2
       in S.length s1 == S.length s2
 
 -- ed25519 has a sig length of 64
 signLength2 :: ByteString -> Property
 signLength2 xs
   = keypairProp $ \(_,sk) ->
-      (64 == S.length (unSignature $ sign' sk xs))
+      (64 == S.length (unSignature $ dsign sk xs))
 
 --------------------------------------------------------------------------------
 -- Driver
