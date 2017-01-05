4c10ed5d89e1419ccbf0e9dfcbf1c4c4ed640f4e
Austin Seipp <aseipp@pobox.com>
2017-01-05 14:29:17 -0600

    tests/hlint: fix when using stack, new-build, etc
    The hlint test previously used a hard-coded hack to find cabal_macros.h,
    so hlint could properly run over the source code. This was an old
    byproduct of the testing infrastructure being born originally from
    Edward Kmett.
    
    Now, the hlint test uses filemanip to find the cabal_macros.h file
    manually, so it should always Do The Right Thing, or fail.
    
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- tests/hlint.hs
+++ tests/hlint.hs
@@ -7,11 +7,25 @@ import           Language.Haskell.HLint
 import           System.Environment
 import           System.Exit
 
+import           System.FilePath.Find (find, fileName)
+
 main :: IO ()
 main = do
-    args <- getArgs
-    hints <- hlint $ [ "src", "benchmarks", "tests"
-                     , "--cpp-define=HLINT"
-                     , "--cpp-file=dist/build/autogen/cabal_macros.h"
-                     ] `mappend` args
-    unless (null hints) exitFailure
+  args <- getArgs
+
+  -- When we build, we might use cabal, stack, cabal new-build, etc..
+  -- so find the cabal_macros.h file dynamically.
+  macros <- find (return True) ((== "cabal_macros.h") `liftM` fileName) "."
+  case macros of
+    [] -> do
+      putStrLn "Couldn't find cabal_macros.h!"
+      exitFailure
+    [cabal_macros] -> do
+      hints <- hlint $ [ "src", "benchmarks", "tests"
+                       , "--cpp-define=HLINT"
+                       , "--cpp-file=" `mappend` cabal_macros
+                       ] `mappend` args
+      unless (null hints) exitFailure
+    _ -> do
+      putStrLn "I found more than one cabal_macros.h file?!?! Bailing!"
+      exitFailure
